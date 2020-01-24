763e8efce5c35e7470ea8acead8a4f4ff97a1fd2
Aaron Minyard <amnyrrd@gmail.com>
2020-01-24 18:29:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   # puts "Guess a number from 1 to 6"
   num = rand(6) + 1
   input = gets.chomp
-  if num == input
+  if input == num.to_s
     p "You guessed the correct number!"
   elsif input == "exit"
     p "Goodbye!"
