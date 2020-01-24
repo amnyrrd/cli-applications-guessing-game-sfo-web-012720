04770c0ab1ad3e67d3cca4780e7eebc7a4d9bff1
Aaron Minyard <amnyrrd@gmail.com>
2020-01-24 18:11:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,13 @@
 def run_guessing_game
   num = rand(6) + 1
   input = gets.chomp
+  if input == "exit"
+    return "Goodbye!"
+  elsif input != num
+    return "Sorry! The computer guessed #{num}."
+  elsif input == num
+    return "You guessed the correct number!"
+  else 
+    return "Not a valid input"
+  end
 end
