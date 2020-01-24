1aef151695a73dfa49381c50ce65a88a74c4da1b
Aaron Minyard <amnyrrd@gmail.com>
2020-01-24 18:22:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
-  else
+  else input != num
     puts "Sorry! The computer guessed #{num}."
   end
 end
