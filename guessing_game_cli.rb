657c3a43d2e91a7ce9ca59f42314882745f91bc6
Aaron Minyard <amnyrrd@gmail.com>
2020-01-24 18:23:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,11 +4,11 @@ def run_guessing_game
   # puts "Guess a number from 1 to 6"
   input = gets.chomp.to_s
   if num == input
-    return "You guessed the correct number!"
+    p "You guessed the correct number!"
   elsif input == "exit"
-    return "Goodbye!"
+    p "Goodbye!"
   else input != num
-    return "Sorry! The computer guessed #{num}."
+    p "Sorry! The computer guessed #{num}."
   end
 end
 
